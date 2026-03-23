38a50c11a4d0dc2e60b55e03138e8cb01f9ebc0c
Adam Cwiek <lohisynths@gmail.com>
2026-03-23 12:09:24 +0100

    uart added

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
+#include <stdio.h>
 
 #define LED0_NODE DT_ALIAS(led0)
 
@@ -13,26 +13,35 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
-	int ret;
+    int ret;
+    uint32_t blink_count = 0U;
 
-	if (!gpio_is_ready_dt(&led)) {
-		printk("Error: LED GPIO device is not ready\n");
-		return 0;
-	}
+    if (!gpio_is_ready_dt(&led)) {
+        printf("Error: LED GPIO device is not ready\n");
+        return 0;
+    }
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
-	if (ret < 0) {
-		printk("Error: failed to configure LED pin (%d)\n", ret);
-		return 0;
-	}
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+    if (ret < 0) {
+        printf("Error: failed to configure LED pin (%d)\n", ret);
+        return 0;
+    }
 
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			printk("Error: failed to toggle LED (%d)\n", ret);
-			return 0;
-		}
+    printf("\r\nBassline Junkie Interface\r\n");
+    printf("Console TX ready on ttyACM0.\r\n");
 
-		k_msleep(500);
-	}
+    while (1) {
+        ret = gpio_pin_toggle_dt(&led);
+        if (ret < 0) {
+            printf("Error: failed to toggle LED (%d)\n", ret);
+            return 0;
+        }
+
+        blink_count++;
+        if ((blink_count % 10U) == 0U) {
+            printf("Heartbeat: LED blink running\r\n");
+        }
+
+        k_msleep(500);
+    }
 }
