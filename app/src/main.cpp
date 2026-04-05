2bce8fdb51282cacffff0a671389aea78c54f741
Adam Cwiek <napent@gmail.com>
2026-04-05 19:28:06 +0200

    Preview volume changes on the LED display
    - Bind `VOL` to the shared display in `main.cpp`
    - Show the scaled knob value as a preview when volume changes
    - Update README and app docs to reflect the new behavior

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -95,6 +95,7 @@ static void input_thread(void *p1, void *, void *) {
     vol.init(midi, leds, inputs);
     led_disp.init(midi, leds, inputs);
     flt.bind_display(led_disp);
+    vol.bind_display(led_disp);
     preset.init(eeprom, led_disp, adsr, flt, lfo, mod, osc);
 
     while (1) {
